bed8f59e79ffe3695ac39481f2d8ab536a3780a0
ferdihatami <ferdihatami@gmail.com>
2024-02-15 21:01:09 +0800

    add home 3

--- components/layoutHome/section/Home3.jsx
+++ components/layoutHome/section/Home3.jsx
@@ -1,7 +1,34 @@
 import React from "react";
+import Image from "next/image";
 
 const Home3 = () => {
-  return <div>Home3</div>;
+  return (
+    <div className="md:grid md:grid-cols-2 space-x-3 lg:p-8 xl:mx-16 lg:mx-0 mx-5 ">
+      <div>
+        <Image
+          src="/Image/banner2.png"
+          width={700}
+          height={700}
+          alt="Wedding invitation"
+        />
+      </div>
+      <div className="">
+        <div className="xl:mt-[130px] lg:mt-[110px] md:mt-[85px] ">
+          <p className="font-black xl:text-[2.6rem] lg:text-[2.1rem] md:text-[1.5rem] text-[2rem] font-[roboto] tracking-[2px]">
+            Bikin Undangan Online Dimanapun dan Kapanpun Cukup Dengan HP Kamu
+          </p>
+        </div>
+        <div className="mt-10 mb-5">
+          <p className="text-justify">
+            Bikin undangan online praktis, kapanpun dan dimanapun. Tanpa harus
+            menggunakan laptop berjam-jam. Cukup dengan hp kamu, pilih tema
+            sesuai keinginanmu, edit detail acara, upload foto terbaikmu dan
+            akan selesai dalam hitungan menit.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home3;
