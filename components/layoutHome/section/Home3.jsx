7556ab22b41bfe7e2380cb323a0b24e04c7aca36
ferdihatami <ferdihatami@gmail.com>
2024-02-18 01:48:23 +0800

    Perbaikan

--- components/layoutHome/section/Home3.jsx
+++ components/layoutHome/section/Home3.jsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 const Home3 = () => {
   return (
-    <div className="bg-red-400">
+    <div className="bg-red-500 py-10 md:py-0">
       <div className="md:grid md:grid-cols-2 space-x-3 lg:p-8 xl:mx-16 lg:mx-0 mx-5 text-white ">
         <div>
           <Image
